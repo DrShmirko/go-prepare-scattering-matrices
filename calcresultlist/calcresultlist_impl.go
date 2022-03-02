1fab92c996aba7c5b9de4c57167bd96721836322
Dr. Konstantin Shmirko <kshmirko@gmail.com>
2022-03-02 23:49:49 +1000

    Добавлены функии обхода в прямом и обратном направлениях

--- calcresultlist/calcresultlist_impl.go
+++ calcresultlist/calcresultlist_impl.go
@@ -16,10 +16,32 @@ func NewCalcResultsList(prefix string) *CalcResultsList {
 	}
 }
 
-func (c *CalcResultsList) Apply(fun func(cr *calcresult.CalculusResult)) error {
-	for tmp := c.Front(); tmp != nil; tmp = tmp.Next() {
+// ApplyForward - apply function fun to every element of a list from the first element to the end.
+func (c *CalcResultsList) ApplyForward(fun func(cr *calcresult.CalculusResult)) error {
+
+	if c.Front() == nil {
+		return fmt.Errorf("applyforward error: list is empty")
+	}
+	tmp := c.Front()
+	for tmp != nil {
 		p := tmp.Value() //.(*CalculusResult)
 		fun(p)
+		tmp = tmp.Next()
+	}
+	return nil
+}
+
+// ApplyBackward - apply function fun to every element of a list from the end element to the first.
+func (c *CalcResultsList) ApplyBackward(fun func(cr *calcresult.CalculusResult)) error {
+
+	if c.Front() == nil {
+		return fmt.Errorf("applybackward error: list is empty")
+	}
+	tmp := c.Tail()
+	for tmp != nil {
+		p := tmp.Value() //.(*CalculusResult)
+		fun(p)
+		tmp = tmp.Prev()
 	}
 	return nil
 }
@@ -27,7 +49,7 @@ func (c *CalcResultsList) Apply(fun func(cr *calcresult.CalculusResult)) error {
 // SaveResults - сохрняем весь список, каждый элемент в свой собственный файл
 func (c *CalcResultsList) SaveResults() error {
 
-	err := c.Apply(func(cr *calcresult.CalculusResult) {
+	err := c.ApplyForward(func(cr *calcresult.CalculusResult) {
 		fname := fmt.Sprintf("out/%s_%05d.out", c.prefix, cr.RecordId)
 		saveto := fmt.Sprintf("pic/%s_%05d.pdf", c.prefix, cr.RecordId)
 		fout, err := os.Create(fname)
